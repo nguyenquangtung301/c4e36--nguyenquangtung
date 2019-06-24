857c388a649153aa66516fc3273b0103c2f90692
MAC OSX <macosx@MACs-iMac.local>
2019-06-24 16:28:54 +0700

    .

--- session1/index.js
+++ session1/index.js
@@ -1,3 +1,8 @@
-let r = prompt("Nhap ban kinh hinh tron ");
+let r = prompt("Nhap r ");
 let s = r*r*3.14
-console.log("area =", s);
+console.log("area =", s );
+ 
+let c = prompt ("Nhap do c");
+let f = c*1.8+32;
+console.log(c, "(c) = ", f , "(f)" );
+
