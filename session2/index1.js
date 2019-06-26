edffba1763324cf3244dd50b1188cd0461bbc1db
MAC OSX <macosx@MACs-iMac.local>
2019-06-27 01:29:46 +0700

    .

---
+++ session2/index1.js
@@ -0,0 +1,43 @@
+// const x = Math.floor(Math.random() * 100) + 1;
+// for(;;){
+// let answer = prompt("your answer");
+// if (answer < x){
+//     console.log("too small");
+// } else if (answer > x){
+//     console.log("toobig");
+// } else {
+//     console.log("bingo");
+// break;
+    
+// }
+// }
+
+// let low = 0;
+// let high = 100;
+// for(;;){
+// let mid = Math.floor((low + high)/2);
+// console.log(mid);
+// let answer = prompt("answer?");
+// if(answer === "s"){
+//     low = mid;
+// } else if (answer === "l"){
+//     high = mid;
+// } elsee if (answer === "c" ){
+//     console.log("i knew it");
+//     break;
+// }
+// }
+// let low = 0;
+// let high = 100;
+// for(;;) {
+// let mid = Math.floor((low + high)/2);
+// let answer = prompt("ANWER???").toLowerCase();
+// if (answer === "s") {
+//     low = mid;
+// } else if (answer === "l") {
+//     high = mid;
+// } else if (answer === "c") {
+//     console.log("I knew IT!");
+//     break;
+// }
+// }
