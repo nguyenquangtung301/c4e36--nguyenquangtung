690ceabb7fc31ec64a9e75ddae30cd34ac868875
MAC OSX <macosx@MACs-iMac.local>
2019-07-25 09:31:21 +0700

    .

--- session/dom.js
+++ session/dom.js
@@ -1,14 +1,38 @@
-const a = document.getElementById('demo-a');
-const btnDemo = document.getElementById('btn-demo');
-const inputDemo = document.getElementById('demo-input');
-btnDemo.onclick = () => {
-    inputDemo.value = a.href;
+const context = {
+    employees: null,
 }
 
+window.onload = async() => {
+    await getEmployees();
+    renderEmployees();
+    viewSalary();
+}
 
-const btnRemove = document.getElementById('btn-remove');
-const dropdownList = document.getElementById('dropdown-list');
-btnRemove.onclick = () => {
-    let arr = [...dropdownList.children];
-    arr.filter(e => e.innerHTML === dropdownList.value)[0].remove();
+const getEmployees = async() => {
+    const res = await fetch('http://dummy.restapiexample.com/api/v1/employees');
+    const employees = await res.json();
+    context.employees = employees;
+}
+
+const renderEmployees = () => {
+    console.dir(context.employees);
+    const listEmployees = document.getElementById('list-employees');
+    context.employees.forEach((employee, index) => {
+        let employeeElement = `<li>${employee.employee_name}</li>`;
+        listEmployees.innerHTML += employeeElement;
+    })
+}
+
+const viewSalary = () => {
+    const listEmployees = document.getElementById('list-employees');
+    const salaryEmployee = document.getElementById('employee_salary');
+    for (let i = 0; i < context.employees.length; i++) {
+        listEmployees.children[i].onmouseover = () => {
+            let salaryView = `<p>The salary of ${context.employees[i].employee_name}: ${context.employees[i].employee_salary}</p>`;
+            salaryEmployee.innerHTML += salaryView;
+        }
+        listEmployees.children[i].onmouseout = () => {
+            salaryEmployee.innerHTML = '';
+        }
+    }
 }
--- session1/index.js
+++ session1/index.js
@@ -1,1 +1,45 @@
+const context={
+    user: null,
+    
+}
+window.onload = async ()=>{
+ await getUsers();
+ renderUsers();
+ viewUser();
+}
+const getUsers = async ()=>{
+    const res = await fetch('https://reqres.in/api/users');
+    const  users= await res.json();
+    context.users = users.data;
+}
 
+const renderUsers = ()=>{
+    let userDiv = document.getElementById('container')
+    context.users.forEach((user, index)=>{
+        console.log(user);
+        let userElement =
+        `
+        <div>
+        <img src=${users.avatar}>
+        <b>Full name:</b>${user.last_name}${user.fisrt_name}
+        <b>Email:</b>${user.email}
+        <div>
+        `
+        userDiv.innerHTML += userElement;
+
+    })
+}
+const viewUser = () =>{
+    
+            
+    
+ for (let index = 0; index < context.user.length; index++) {
+     let user = document.getElementById(`user-${index}`);
+    user.addEventListener('click',()=>{
+        userDetail.innerHTML = '';
+        userDetail.innerHTML +=  inner.HTML;
+       
+    }
+    )}
+     
+ }
